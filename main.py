0cbc6c73f9ac479a2f38d9750366ec77320fe947
Edudlufetips1 <aq8829@outlook.com>
2026-01-21 18:25:36 -0500

    updated to generic prompt, verbose response option

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import os
-import agparse
-from dotenv import load_dotenv # pyright: ignore[reportMissingImports]
+from dotenv import load_dotenv
 from google import genai
-
-load_dotenv()
+from google.genai import types
+import argparse
 
 def main():
+    load_dotenv()
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
         raise RuntimeError("GEMINI_API_KEY environment variable not set")
@@ -13,13 +19,16 @@ def main():
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",)
+        contents=messages)
     
     if not response.usage_metadata:
         raise RuntimeError("Gemini API response appears to be malformed")
     
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    if args.verbose:
+        print(f"User prompt:, {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
     print("Response:")
     print(response.text)
 
